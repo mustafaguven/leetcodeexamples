e990f7d48353c8668cbe725d868881951e78ccf1
Mustafa Guven <mustafa.guven@vodafone.com>
2022-05-28 15:28:21 +0300

    cses: Increasing Array

---
+++ LeetCodeExamples/src/main/java/cses/_04_IncreasingArray/IncreasingArray.java
@@ -0,0 +1,45 @@
+package cses._04_IncreasingArray;
+
+import java.util.Arrays;
+
+public class IncreasingArray {
+
+  public final Integer[] array = new Integer[] {3, 2, 5, 1, 7};
+
+  public static void main(String[] args) {
+    var ia = new IncreasingArray();
+    ia.nestedSolution(0, 0, Arrays.stream(ia.array).mapToInt(it -> it).toArray());
+    System.out.println("------");
+    ia.solution(0, Arrays.stream(ia.array).mapToInt(it -> it).toArray());
+  }
+
+  private void solution(int i, int[] array) {
+    var move = 0;
+    while (i < array.length) {
+      if (i == array.length - 1) return;
+      var first = array[i];
+      var second = array[i + 1];
+      if (first > second) {
+        array[i + 1] = second + 1;
+        move++;
+        System.out.println("move: " + move + ", array: " + Arrays.toString(array));
+      } else {
+        i++;
+      }
+    }
+  }
+
+  private void nestedSolution(int i, int move, int[] array) {
+    if (i == array.length - 1) return;
+    var first = array[i];
+    var second = array[i + 1];
+    if (first > second) {
+      array[i + 1] = second + 1;
+      move++;
+      System.out.println("move: " + move + ", array: " + Arrays.toString(array));
+    } else {
+      i++;
+    }
+    nestedSolution(i, move, array);
+  }
+}
